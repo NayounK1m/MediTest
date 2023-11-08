da11067068eaa51de812ed9b07c456897e147d38
KimNayoun <94847818+NayounK1m@users.noreply.github.com>
2023-11-09 01:03:27 +0900

    Alarm UI Update

--- Client/Assets/Script/AlarmClock.cs
+++ Client/Assets/Script/AlarmClock.cs
@@ -2,33 +2,43 @@ using UnityEngine;
 
 public class AlarmClock : MonoBehaviour
 {
-	const string ACTION_SET_ALARM = "android.intent.action.SET_ALARM";
-	const string EXTRA_HOUR = "android.intent.extra.alarm.HOUR";
-	const string EXTRA_MINUTES = "android.intent.extra.alarm.MINUTES";
-	const string EXTRA_MESSAGE = "android.intent.extra.alarm.MESSAGE";
+	//const string ACTION_SET_ALARM = "android.intent.action.SET_ALARM";
+	//const string EXTRA_HOUR = "android.intent.extra.alarm.HOUR";
+	//const string EXTRA_MINUTES = "android.intent.extra.alarm.MINUTES";
+	//const string EXTRA_MESSAGE = "android.intent.extra.alarm.MESSAGE";
 
-	public void AlarmBtn()
-	{
-		print("Hello!");
-		CreateAlarm("My alarm!", 8, 52);
-	}
+	//public void AlarmBtn()
+	//{
+	//	print("Hello!");
+	//	CreateAlarm("My alarm!", 8, 52);
+	//}
 
-	public void CreateAlarm(string message, int hour, int minutes)
-	{
-		var intentAJO = new AndroidJavaObject("android.content.Intent", ACTION_SET_ALARM);
-		intentAJO
-			.Call<AndroidJavaObject>("putExtra", EXTRA_MESSAGE, message)
-			.Call<AndroidJavaObject>("putExtra", EXTRA_HOUR, hour)
-			.Call<AndroidJavaObject>("putExtra", EXTRA_MINUTES, minutes);
+	//public void CreateAlarm(string message, int hour, int minutes)
+	//{
+	//	var intentAJO = new AndroidJavaObject("android.content.Intent", ACTION_SET_ALARM);
+	//	intentAJO
+	//		.Call<AndroidJavaObject>("putExtra", EXTRA_MESSAGE, message)
+	//		.Call<AndroidJavaObject>("putExtra", EXTRA_HOUR, hour)
+	//		.Call<AndroidJavaObject>("putExtra", EXTRA_MINUTES, minutes);
 		
-		GetUnityActivity().Call("startActivity", intentAJO);
-	}
+	//	GetUnityActivity().Call("startActivity", intentAJO);
+	//}
 
-	AndroidJavaObject GetUnityActivity()
-	{
-		using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-		{
-			return unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-		}
-	}
+	//AndroidJavaObject GetUnityActivity()
+	//{
+	//	using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+	//	{
+	//		return unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+	//	}
+	//}
+
+	public void AddBtn()
+    {
+
+    }
+
+	public void DoneBtn()
+    {
+
+    }
 }
